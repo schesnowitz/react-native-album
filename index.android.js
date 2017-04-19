ea2f3084e5cc3a4489e4111aa66ad498c81faf11
schesnowitz <steve@chesnowitz.com>
2017-04-19 06:24:52 -0400

    created and imported AlbumList Component

--- index.android.js
+++ index.android.js
@@ -1,13 +1,19 @@
 
 //import library to assist with creation of Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from 'albums/src/components/header'
-// create the Component
-  const App = () => (
+import { AppRegistry, View } from 'react-native';
+import Header from 'albums/src/components/Header'
+import AlbumList from 'albums/src/components/AlbumList' 
 
-      <Header headerText={'Sexy Records'}/> 
-    );
+// create the Component
+    const App = () => {
+      return (
+        <View>
+          <Header headerText={'Sexy Records'}/>
+          <AlbumList />
+        </View>
+      );
+    };
 
 
 // send it to the device     albums is the name of the app and App is our Component  "const App"
