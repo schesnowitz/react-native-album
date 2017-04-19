04c841a2a3fb11fae43d6828ec64ed3548da3739
schesnowitz <steve@chesnowitz.com>
2017-04-19 06:34:40 -0400

    converted AlbumList to class based component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  // const { textStyle, viewStyle } = styles;
+  class AlbumList extends Component {
+    // const { textStyle, viewStyle } = styles;
+    render() {
+      return (
+          <View>
+            <Text>Album List</Text>
+          </View>
 
-  return (
-      <View>
-        <Text>Album List</Text>
-      </View>
-
-    );
-  };
+      );
+    }
+  } // classes do not requre semi-colon to close //
 
   export default AlbumList;
