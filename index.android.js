aae1472d010e8dd2f94c19e21757961b347a7b9f
schesnowitz <steve@chesnowitz.com>
2017-04-19 04:21:59 -0400

    rendering text on screen

--- index.android.js
+++ index.android.js
@@ -1,53 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+//import library to assist with creation of Component
 
-export default class albums extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+// create the Component
+  const App = () => (
+      <Text>This is in a text tag</Text>
     );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('albums', () => albums);
+// send it to the device     albums is the name of the app and App is our Component  "const App"
+AppRegistry.registerComponent('albums', () => App);
