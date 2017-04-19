b17f0a60ce639f8064fd8ea2b69bcefee8114daa
schesnowitz <steve@chesnowitz.com>
2017-04-19 08:06:23 -0400

    listing records from api

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
-  class AlbumList extends Component {
 
+  class AlbumList extends Component {
+    state = { albums: [] }; //set initial state
     componentWillMount() {
-      console.warn("is component mounting?");
+      // console.warn("is component mounting?");
+      axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        // .then(response => console.warn(response));
+        .then(response => this.setState({
+          albums: response.data
+        }));
+    }
+    renderAlbums() { //this is called insud the render
+      return this.state.albums.map( album => <Text>{album.title}</Text>); //.map is an array helper fat arrow gets single album
     }
 
     render() {
+        // console.warn(this.state);
       return (
           <View>
-            <Text>Album List</Text>
+            <Text>{this.renderAlbums()}</Text> 
           </View>
 
       );
